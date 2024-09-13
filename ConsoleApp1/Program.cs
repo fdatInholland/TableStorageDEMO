a1c46b5d972209c9e6bf622d4d1161f6dc81c88f
Frank Dersjant <36342252+frankdersjant@users.noreply.github.com>
2024-09-13 16:54:04 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using Azure;
+using Azure.Data.Tables;
+using Azure.Storage.Blobs.Models;
+using Domain;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private const string TableName = "Product";
+        private static readonly string ConnectionString = "UseDevelopmentStorage=true";
+
+        static async Task Main(string[] args)
+        {
+            // Create a new TableClient instance
+            var serviceClient = new TableServiceClient(ConnectionString);
+            var tableClient = serviceClient.GetTableClient(TableName);
+
+            // Create a table if it doesn't exist
+            await tableClient.CreateIfNotExistsAsync();
+
+            //Insert new product
+            Product newproduct = new Product("Electronics", "12345", "Laptop", 999.99m);
+            await tableClient.AddEntityAsync(newproduct).ConfigureAwait(false);
+
+            // Retrieve the entity
+
+            var retrievedProduct = await tableClient.GetEntityAsync<Product>("Electronics", "12345");
+            Console.WriteLine($"Retrieved product: {retrievedProduct.Value.Name}, Price: {retrievedProduct.Value.Price.ToString()}");
+
+
+            // Update the entity
+            Product updateProduct = new Product("Electronics", "12345", "Laptop", 899.99m);
+            await tableClient.UpdateEntityAsync(updateProduct, ETag.All, TableUpdateMode.Replace);
+
+            //filtering
+            var filter = TableClient.CreateQueryFilter($"Price gt {400}");
+
+            // Execute the query
+            var entities = tableClient.QueryAsync<Product>(filter);
+
+            var result = new List<Product>();
+            await foreach (var entity in entities)
+            {
+                result.Add(entity);
+            }
+        }
+    }
+}
---
+++ Domain/Product.cs
@@ -0,0 +1,58 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using Microsoft.WindowsAzure.Storage;
+
+namespace Domain
+{
+    public class Product : ITableEntity
+    {
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        //required for deserialisation
+        public Product()
+        {
+
+        }
+
+        public Product(string partitionKey, string rowKey, string name, decimal price)
+        {
+            PartitionKey = partitionKey;
+            RowKey = rowKey;
+            Name = name;
+            Price = price;
+        }
+
+        // Required for ITableEntity
+        public void ReadEntity(IDictionary<string, object> properties, OperationContext operationContext)
+        {
+            PartitionKey = properties[nameof(PartitionKey)].ToString();
+            RowKey = properties[nameof(RowKey)].ToString();
+            Timestamp = (DateTimeOffset?)properties[nameof(Timestamp)];
+            ETag = (ETag)properties[nameof(ETag)];
+
+            Name = properties[nameof(Name)].ToString();
+            Price = Convert.ToDecimal(properties[nameof(Price)]);
+        }
+
+        public IDictionary<string, object> WriteEntity(OperationContext operationContext)
+        {
+            var properties = new Dictionary<string, object>
+        {
+            { nameof(PartitionKey), PartitionKey },
+            { nameof(RowKey), RowKey },
+            { nameof(Timestamp), Timestamp },
+            { nameof(ETag), ETag },
+            { nameof(Name), Name },
+            { nameof(Price), Price }
+        };
+
+            return properties;
+        }
+    }
+}
